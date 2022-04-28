23cd6688bfb51080f73035159d8019d3c1d54021
Raphael Dohnalek <if20b075@technikum-wien.at>
2022-04-28 22:28:50 +0200

    Add rotation with joystick

--- CYBERCOLOR/Assets/Scripts/Player/PlayerMovement.cs
+++ CYBERCOLOR/Assets/Scripts/Player/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private Vector2 _movementInput;
     [SerializeField] private Vector3 _moveVector;
+    [SerializeField] private Vector2 _rotateInput;
 
     private Rigidbody _rigidbody;
 
@@ -19,12 +20,25 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 3);
+    }
+
     void FixedUpdate()
     {
         _moveVector = new Vector3(_movementInput.x, 0, _movementInput.y) * Speed * SpeedMultiplier * Time.fixedDeltaTime;
-        //_rigidbody.AddForce(_moveVector, ForceMode.Acceleration);
         _rigidbody.velocity = _moveVector;
+
+        // Prevents reset of rotation to vector 0, 0 (0 degrees angle)
+        if(_rotateInput.x != 0f && _rotateInput.y != 0f)
+        {
+            float angle = Mathf.Atan2(_rotateInput.x, _rotateInput.y) * Mathf.Rad2Deg;
+            _rigidbody.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context) => _movementInput = context.ReadValue<Vector2>();
+
+    public void OnRotate(InputAction.CallbackContext context) => _rotateInput = context.ReadValue<Vector2>();
 }
