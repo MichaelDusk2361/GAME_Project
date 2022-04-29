f736f8577d59b1e60db1128481b712dd83f5a100
Raphael Dohnalek <r.dohnalek@gmx.at>
2022-04-29 17:46:26 +0200

    Add hole teleport

--- CYBERCOLOR/Assets/Scripts/Levels/Hole.cs
+++ CYBERCOLOR/Assets/Scripts/Levels/Hole.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private Dictionary<PlayerMovement, Vector3> _fallingPlayers = new();
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.transform.GetComponent<PlayerMovement>() is PlayerMovement player)
+        if (other.transform.GetComponent<PlayerMovement>() is PlayerMovement player)
         {
-            StartCoroutine(player.OnStun(1.5f));
+            if (_fallingPlayers.ContainsKey(player))
+            {
+                StartCoroutine(StunPlayer(1.5f, player, _fallingPlayers[player]));
+            }
+            else
+            {
+                player.Stunned = true;
+                _fallingPlayers.Add(player, player.transform.position);
+            }
         }
+    }
+
+    public IEnumerator StunPlayer(float stunDuration,
+        PlayerMovement player, Vector3 entryPosition)
+    {
+        Vector3 direction = entryPosition - transform.position;
+
+        Vector3 position = transform.position + direction + direction.normalized * 2;
+        position.y = 1;
+        player.transform.position = position;
+
+
+        yield return new WaitForSeconds(stunDuration);
+        player.Stunned = false;
+
+        _fallingPlayers.Remove(player);
     }
 }
--- CYBERCOLOR/Assets/Scripts/Player/PlayerMovement.cs
+++ CYBERCOLOR/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 5f;
     public float SpeedMultiplier = 10f;
+    public bool Stunned;
 
     [Header("Debugging Variables")]
-    [SerializeField] private bool _stunned;
     [SerializeField] private Vector2 _movementInput;
     [SerializeField] private Vector3 _moveVector;
     [SerializeField] private Vector2 _rotateInput;
@@ -30,11 +30,11 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_stunned)
+        if (Stunned)
             return;
 
         _moveVector = new Vector3(_movementInput.x, 0, _movementInput.y) * Speed * SpeedMultiplier * Time.fixedDeltaTime;
-        _rigidbody.AddForce(_moveVector, ForceMode.Acceleration);
+        _rigidbody.AddForce(_moveVector, ForceMode.VelocityChange);
 
         // Prevents reset of rotation to vector 0, 0 (0 degrees angle)
         if(_rotateInput.x != 0f && _rotateInput.y != 0f)
@@ -42,13 +42,6 @@ public class PlayerMovement : MonoBehaviour
             float angle = Mathf.Atan2(_rotateInput.x, _rotateInput.y) * Mathf.Rad2Deg;
             _rigidbody.rotation = Quaternion.Euler(0f, angle, 0f);
         }
-    }
-
-    public IEnumerator OnStun(float seconds)
-    {
-        _stunned = true;
-        yield return new WaitForSeconds(seconds);
-        _stunned = false;
     }
 
     public void OnMove(InputAction.CallbackContext context) => _movementInput = context.ReadValue<Vector2>();
