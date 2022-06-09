ba4323cc6ad48dc114b7b22a3ce7fcb2f4b8ab41
Michael Duskanich <michael.duskanich@gmail.com>
2022-06-09 03:02:31 +0200

    Improve movement

--- CYBERCOLOR/Assets/Scripts/Player/PlayerMovement.cs
+++ CYBERCOLOR/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,15 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed = 5f;
-    public float MaxSpeed = 5f;
-    [SerializeField] private bool _stunned;
+    [SerializeField] private float _maxSpeed = 5f;
+    [SerializeField] private float _acceleration = 5f;
+    [SerializeField] private float _decceleration = 5f;
+    private bool _stunned = false;
+    private bool _grounded = false;
+    [SerializeField] private GameObject _groundCheckGameObject;
 
-    [Header("Debugging Variables")]
-    [SerializeField] private Vector2 _movementInput;
-    [SerializeField] private Vector3 _moveVector;
-    [SerializeField] private Vector2 _rotateInput;
+    private Vector2 _movementInput;
+    private Vector2 _rotateInput;
 
     private Rigidbody _rigidbody;
     private PlayerProjectileController _projectileController;
@@ -33,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Debug.DrawLine(transform.position, transform.position + transform.forward * 3);
+        _grounded = Physics.Raycast(_groundCheckGameObject.transform.position, Vector3.down, 1);
     }
 
     void FixedUpdate()
@@ -41,16 +42,17 @@ public class PlayerMovement : MonoBehaviour
         if (Stunned)
             return;
 
-        _moveVector = Vector3.ClampMagnitude(new Vector3(_movementInput.x, 0, _movementInput.y), 1) * Speed * Time.fixedDeltaTime;
-        if (_projectileController.IsCharging)
-            _moveVector *= 0.1f;
-        _rigidbody.AddForce(_moveVector, ForceMode.VelocityChange);
-
-        _rigidbody.velocity = new Vector3(
-            Mathf.Clamp(_rigidbody.velocity.x, -MaxSpeed, MaxSpeed),
-            _rigidbody.velocity.y,
-            Mathf.Clamp(_rigidbody.velocity.z, -MaxSpeed, MaxSpeed));
-
+        if (_grounded)
+        {
+            // some big math
+            Vector3 movementInput = Vector3.ClampMagnitude(new Vector3(_movementInput.x, 0, _movementInput.y), 1);
+            float maxMovementSpeed = (_projectileController.IsCharging ? _maxSpeed * 0.4f : _maxSpeed);
+            Vector3 targetSpeed = movementInput * maxMovementSpeed;
+            Vector3 speedDif = targetSpeed - _rigidbody.velocity;
+            float accelerationRate = targetSpeed.magnitude > 0.01f ? _acceleration : _decceleration;
+            Vector3 movement = speedDif.magnitude * accelerationRate * speedDif.normalized;
+            _rigidbody.AddForce(new Vector3(movement.x, 0, movement.z));
+        }
         // Prevents reset of rotation to vector 0, 0 (0 degrees angle)
         if (_rotateInput.x != 0f || _rotateInput.y != 0f)
         {
@@ -61,7 +63,8 @@ public class PlayerMovement : MonoBehaviour
 
     public void Knockback(Vector3 forceDir, float scale)
     {
-        _rigidbody.AddForce((new Vector3(forceDir.x, 0.1f, forceDir.z)).normalized*30f* scale, ForceMode.Impulse);
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.AddForce((new Vector3(forceDir.x, 0.1f, forceDir.z)).normalized * 30f * scale, ForceMode.Impulse);
     }
 
     public void OnMove(InputAction.CallbackContext context) => _movementInput = context.ReadValue<Vector2>();
